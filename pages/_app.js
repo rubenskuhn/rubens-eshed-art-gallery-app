496c8c8db57803f32e1ed54288b3baec65ce4549
rubenskuhn <rubenskuhn@gmail.com>
2024-02-05 16:28:14 +0100

    task2 done

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,19 @@
 import GlobalStyle from "../styles";
+import useSWR from "swr";
 
 export default function App({ Component, pageProps }) {
+  const fetcher = (...args) => fetch(...args).then((res) => res.json());
+
+  const { data, error, isLoading } = useSWR(
+    "https://example-apis.vercel.app/api/art",
+    fetcher
+  );
+  if (error) return <div>Failed to load, Dumb-dumb</div>;
+  if (isLoading) return <div>Wait! Don't shoot! Loading...</div>;
   return (
     <>
       <GlobalStyle />
-      <Component {...pageProps} />
+      <Component {...pageProps} data={data} />
     </>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,31 @@
-import useSWR from "swr";
 import ArtPieces from "@/components/ArtPieces";
+// import spotlightPage from "@/components/SpotLight";
 
-export default function HomePage() {
+export default function HomePage({ data }) {
   const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
-  const { data, error, isLoading } = useSWR(
-    "https://example-apis.vercel.app/api/art",
-    fetcher
-  );
-  console.log(data);
+  console.log("==========", data);
 
-  if (error) return <div>Failed to load, Dumb-dumb</div>;
-  if (isLoading) return <div>Wait! Don't shoot! Loading...</div>;
+  // const { data, error, isLoading } = useSWR(
+  //   "https://example-apis.vercel.app/api/art",
+  //   fetcher
+  // );
+  // console.log(data);
+
+  // if (error) return <div>Failed to load, Dumb-dumb</div>;
+  // if (isLoading) return <div>Wait! Don't shoot! Loading...</div>;
 
   // map and render data
 
   return (
     <div>
-      <h1>Vomit... or Modern Art if you must</h1>
+      <h1>Art Gallery</h1>
 
+      <h2>Spot Light of the Day:</h2>
+      <spotlightPage data={data} />
+
+      <h3>Other garbage:</h3>
       <ArtPieces data={data} />
-
     </div>
   );
 }
