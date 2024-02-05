f2840ae3cf22435d4d6d0049d216efd649dd504a
rubenskuhn <rubenskuhn@gmail.com>
2024-02-05 11:12:42 +0100

    swr 2

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,23 @@
+import useSWR from "swr";
+
 export default function HomePage() {
+  const fetcher = (...args) => fetch(...args).then((res) => res.json());
+
+  const { data, error, isLoading } = useSWR(
+    "https://example-apis.vercel.app/api/art",
+    fetcher
+  );
+  console.log(data);
+
+  if (error) return <div>Failed to load, Dumb-dumb</div>;
+  if (isLoading) return <div>Wait! Don't shoot! Loading...</div>;
+
+  // render data
+  // return <div>hello {data.name}!</div>;
+
   return (
     <div>
-      <h1>Hello from Next.js</h1>
+      <h1>F#ck off & Die.js</h1>
     </div>
   );
 }
