f25a9344d21e0acb7d15197ac912504ec1ece91f
YOUR NAME <94797132+PARAMET7@users.noreply.github.com>
2024-02-05 17:40:42 +0100

    finishing the 2 components

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,27 @@
-import GlobalStyle from "../styles";
+import useSWR from "swr";
+import "@mantine/core/styles.css";
+import { createTheme, MantineProvider, GlobalStyles } from "@mantine/core";
+// import Layout from "@/components/Layout";
+
+const theme = createTheme({
+  fontFamily: "Open Sans, sans-serif",
+  primaryColor: "cyan",
+});
+
+const API_URL = "https://example-apis.vercel.app/api/art";
+const fetcher = (url) => fetch(url).then((response) => response.json());
 
 export default function App({ Component, pageProps }) {
+  const { data, error, isLoading } = useSWR(API_URL, fetcher);
+
+  if (isLoading) return <h1>Loading...</h1>;
+  if (error) return <h1>Error...</h1>;
+
   return (
-    <>
-      <GlobalStyle />
-      <Component {...pageProps} />
-    </>
+    <MantineProvider theme={theme} defaultColorScheme="dark">
+
+        <Component {...pageProps} globalData={data} />
+
+    </MantineProvider>
   );
 }
