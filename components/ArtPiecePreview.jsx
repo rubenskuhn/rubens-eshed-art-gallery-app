35a5daec3ed82a2cabd805d33eb90e21e203a038
rubenskuhn <rubenskuhn@gmail.com>
2024-02-06 15:40:01 +0100

    passing the torch task 4

---
+++ components/ArtPiecePreview.jsx
@@ -0,0 +1,26 @@
+import { Card, Image, Text, Group, RingProgress } from "@mantine/core";
+import classes from "./ArtPiecePreview.module.css";
+import Link from "next/link";
+
+export function ArtPiecePreview({ image, title = "", artist, slug }) {
+  return (
+    <Link href={`/art-pieces/${slug}`}>
+      <Card withBorder padding="lg" className={classes.card}>
+        <Card.Section>
+          <Image src={image} alt={title} height={400} />
+        </Card.Section>
+
+        <Group justify="space-between" mt="xl">
+          <Text fz="sm" fw={700} className={classes.title}>
+            {title}
+          </Text>
+          <Group gap={5}>
+            <Text fz="sm" fw={700}>
+              {artist}
+            </Text>
+          </Group>
+        </Group>
+      </Card>
+    </Link>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -2,21 +2,6 @@ import ArtPieces from "@/components/ArtPieces";
 // import spotlightPage from "@/components/SpotLight";
 
 export default function HomePage({ data }) {
-  const fetcher = (...args) => fetch(...args).then((res) => res.json());
-
-  console.log("==========", data);
-
-  // const { data, error, isLoading } = useSWR(
-  //   "https://example-apis.vercel.app/api/art",
-  //   fetcher
-  // );
-  // console.log(data);
-
-  // if (error) return <div>Failed to load, Dumb-dumb</div>;
-  // if (isLoading) return <div>Wait! Don't shoot! Loading...</div>;
-
-  // map and render data
-
   return (
     <div>
       <h1>Art Gallery</h1>
