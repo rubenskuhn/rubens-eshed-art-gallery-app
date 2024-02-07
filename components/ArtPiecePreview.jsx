9328e68c2332ee69cabe7131290332e002b09149
YOUR NAME <94797132+PARAMET7@users.noreply.github.com>
2024-02-07 09:44:48 +0100

    the button is working but there no image:

--- components/ArtPiecePreview.jsx
+++ components/ArtPiecePreview.jsx
@@ -1,8 +1,10 @@
 import { Card, Image, Text, Group, RingProgress } from "@mantine/core";
 import classes from "./ArtPiecePreview.module.css";
 import Link from "next/link";
+import FavoriteButton from "./FavoriteButton.js";
 
-export function ArtPiecePreview({ image, title = "", artist, slug }) {
+
+export function ArtPiecePreview({ image, title = "", artist, slug, isFavorite }) {
   return (
     <Link href={`/art-pieces/${slug}`}>
       <Card withBorder padding="lg" className={classes.card}>
@@ -13,11 +15,13 @@ export function ArtPiecePreview({ image, title = "", artist, slug }) {
         <Group justify="space-between" mt="xl">
           <Text fz="sm" fw={700} className={classes.title}>
             {title}
+            <FavoriteButton isFavorite={isFavorite} />
           </Text>
           <Group gap={5}>
             <Text fz="sm" fw={700}>
               {artist}
             </Text>
+
           </Group>
         </Group>
       </Card>
--- pages/[slug].jsx
+++ pages/[slug].jsx
@@ -1,16 +1,24 @@
 import { useRouter } from "next/router";
 import Image from "next/image";
 import ArtPieceDetails from "@/components/ArtPieceDetails";
+import data from "pages/_app.js";
+import Link from "next/link";
+
+import Head from "next/head";
+
 
 export default function artPieceDetail() {
   const router = useRouter();
   const { slug } = router.query;
-  const v_d = data.find(({ slug }) => slug === router.query.slug);
-  consolog("============", v_d);
+  console.log("slug: ", slug);
+
+  // const v_d = data.find(({ slug }) => slug === router.query.slug);
+  // console.log("v_d: ", v_d);
 
   return (
     <>
-      <h1>Detailes Of The Piece `${title}`</h1>
+      <h1>Detailes Of The Piece</h1>
+       <image></image>
       <p>Post: {router.query.slug}</p>;
     </>
   );
